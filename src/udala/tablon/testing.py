e79861cdc95e257b0b40de7464c894c529811a93
Lur Ibargutxi <libargutxi@codesyntax.com>
2024-05-23 11:38:43 +0200

    "Create addon: udala.tablon"

---
+++ src/udala/tablon/testing.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import udala.tablon
+
+
+class UdalaTablonLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        import plone.app.dexterity
+        self.loadZCML(package=plone.app.dexterity)
+        import plone.restapi
+        self.loadZCML(package=plone.restapi)
+        self.loadZCML(package=udala.tablon)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'udala.tablon:default')
+
+
+UDALA_TABLON_FIXTURE = UdalaTablonLayer()
+
+
+UDALA_TABLON_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(UDALA_TABLON_FIXTURE,),
+    name='UdalaTablonLayer:IntegrationTesting',
+)
+
+
+UDALA_TABLON_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(UDALA_TABLON_FIXTURE,),
+    name='UdalaTablonLayer:FunctionalTesting',
+)
+
+
+UDALA_TABLON_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        UDALA_TABLON_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name='UdalaTablonLayer:AcceptanceTesting',
+)
---
+++ src/udala/tablon/tests/test_setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from udala.tablon.testing import UDALA_TABLON_INTEGRATION_TESTING  # noqa: E501
+
+import unittest
+
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
+class TestSetup(unittest.TestCase):
+    """Test that udala.tablon is properly installed."""
+
+    layer = UDALA_TABLON_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_product_installed(self):
+        """Test if udala.tablon is installed."""
+        self.assertTrue(self.installer.is_product_installed(
+            'udala.tablon'))
+
+    def test_browserlayer(self):
+        """Test that IUdalaTablonLayer is registered."""
+        from plone.browserlayer import utils
+        from udala.tablon.interfaces import IUdalaTablonLayer
+        self.assertIn(
+            IUdalaTablonLayer,
+            utils.registered_layers())
+
+
+class TestUninstall(unittest.TestCase):
+
+    layer = UDALA_TABLON_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer.uninstall_product('udala.tablon')
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        """Test if udala.tablon is cleanly uninstalled."""
+        self.assertFalse(self.installer.is_product_installed(
+            'udala.tablon'))
+
+    def test_browserlayer_removed(self):
+        """Test that IUdalaTablonLayer is removed."""
+        from plone.browserlayer import utils
+        from udala.tablon.interfaces import IUdalaTablonLayer
+        self.assertNotIn(IUdalaTablonLayer, utils.registered_layers())

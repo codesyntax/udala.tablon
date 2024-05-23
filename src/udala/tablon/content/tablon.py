a3a4c59d5251dfc09c94058d92c6382132d3d6e8
Lur Ibargutxi <libargutxi@codesyntax.com>
2024-05-23 11:42:58 +0200

    "Add content_type: DocumentoTablon"

--- src/udala/tablon/content/tablon.py
+++ src/udala/tablon/content/tablon.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 from plone.app.textfield import RichText
+
 # from plone.autoform import directives
 from plone.dexterity.content import Container
 
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
+from udala.tablon import _
 
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
 # from zope import schema
 from zope.interface import implementer
-
-
-from udala.tablon import _
 
 
 class ITablon(model.Schema):
---
+++ src/udala/tablon/tests/test_ct_documento_tablon.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.dexterity.interfaces import IDexterityFTI
+from udala.tablon.content.documento_tablon import IDocumentoTablon  # NOQA E501
+from udala.tablon.testing import UDALA_TABLON_INTEGRATION_TESTING  # noqa
+from zope.component import createObject
+from zope.component import queryUtility
+
+import unittest
+
+
+class DocumentoTablonIntegrationTest(unittest.TestCase):
+
+    layer = UDALA_TABLON_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.parent = self.portal
+
+    def test_ct_documento_tablon_schema(self):
+        fti = queryUtility(IDexterityFTI, name='DocumentoTablon')
+        schema = fti.lookupSchema()
+        self.assertEqual(IDocumentoTablon, schema)
+
+    def test_ct_documento_tablon_fti(self):
+        fti = queryUtility(IDexterityFTI, name='DocumentoTablon')
+        self.assertTrue(fti)
+
+    def test_ct_documento_tablon_factory(self):
+        fti = queryUtility(IDexterityFTI, name='DocumentoTablon')
+        factory = fti.factory
+        obj = createObject(factory)
+
+        self.assertTrue(
+            IDocumentoTablon.providedBy(obj),
+            u'IDocumentoTablon not provided by {0}!'.format(
+                obj,
+            ),
+        )
+
+    def test_ct_documento_tablon_adding(self):
+        setRoles(self.portal, TEST_USER_ID, ['Contributor'])
+        obj = api.content.create(
+            container=self.portal,
+            type='DocumentoTablon',
+            id='documento_tablon',
+        )
+
+        self.assertTrue(
+            IDocumentoTablon.providedBy(obj),
+            u'IDocumentoTablon not provided by {0}!'.format(
+                obj.id,
+            ),
+        )
+
+        parent = obj.__parent__
+        self.assertIn('documento_tablon', parent.objectIds())
+
+        # check that deleting the object works too
+        api.content.delete(obj=obj)
+        self.assertNotIn('documento_tablon', parent.objectIds())
+
+    def test_ct_documento_tablon_globally_addable(self):
+        setRoles(self.portal, TEST_USER_ID, ['Contributor'])
+        fti = queryUtility(IDexterityFTI, name='DocumentoTablon')
+        self.assertTrue(
+            fti.global_allow,
+            u'{0} is not globally addable!'.format(fti.id)
+        )
+
+    def test_ct_documento_tablon_filter_content_type_false(self):
+        setRoles(self.portal, TEST_USER_ID, ['Contributor'])
+        fti = queryUtility(IDexterityFTI, name='DocumentoTablon')
+        portal_types = self.portal.portal_types
+        parent_id = portal_types.constructContent(
+            fti.id,
+            self.portal,
+            'documento_tablon_id',
+            title='DocumentoTablon container',
+        )
+        self.parent = self.portal[parent_id]
+        obj = api.content.create(
+            container=self.parent,
+            type='Document',
+            title='My Content',
+        )
+        self.assertTrue(
+            obj,
+            u'Cannot add {0} to {1} container!'.format(obj.id, fti.id)
+        )

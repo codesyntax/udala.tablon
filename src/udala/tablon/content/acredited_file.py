189d9239a98f1bdc1b216608126a4eeef079f72d
Lur Ibargutxi <libargutxi@codesyntax.com>
2024-06-06 11:30:09 +0200

    acreditedFile fields

--- src/udala/tablon/content/acredited_file.py
+++ src/udala/tablon/content/acredited_file.py
@@ -3,60 +3,26 @@
 # from plone.autoform import directives
 from plone.dexterity.content import Container
 
-# from plone.namedfile import field as namedfile
+from plone.namedfile.field import NamedBlobFile
+
 from plone.supermodel import model
 
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from udala.tablon import _
+from udala.tablon import _
 
 
 class IAcreditedFile(model.Schema):
     """ Marker interface and Dexterity Python Schema for AcreditedFile
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    file = NamedBlobFile(title="File", required=True)
 
-    # model.load('acredited_file.xml')
+    url = schema.TextLine(title=_(u"Accreditation URL"))
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
 
 
 @implementer(IAcreditedFile)

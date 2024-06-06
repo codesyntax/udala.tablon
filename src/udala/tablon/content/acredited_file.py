ecffa791bf492d41c6a77c31f2c757eaddb840f2
Lur Ibargutxi <libargutxi@codesyntax.com>
2024-06-06 12:22:34 +0200

    "Add view: view"

--- src/udala/tablon/content/acredited_file.py
+++ src/udala/tablon/content/acredited_file.py
@@ -2,18 +2,14 @@
 # from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Container
-
 from plone.namedfile.field import NamedBlobFile
-
 from plone.supermodel import model
+from udala.tablon import _
 
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
-
-
-from udala.tablon import _
 
 
 class IAcreditedFile(model.Schema):
---
+++ src/udala/tablon/views/tablon_view.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# from udala.tablon import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class ITablonView(Interface):
+    """ Marker Interface for ITablonView"""
+
+
+@implementer(ITablonView)
+class TablonView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('tablon_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()

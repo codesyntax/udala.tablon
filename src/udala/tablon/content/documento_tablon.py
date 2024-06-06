219a9e4e215ce2ab1b668ff17c0c61e12623f04b
Lur Ibargutxi <libargutxi@codesyntax.com>
2024-06-06 11:16:23 +0200

    fields

--- src/udala/tablon/content/documento_tablon.py
+++ src/udala/tablon/content/documento_tablon.py
@@ -8,55 +8,25 @@ from plone.supermodel import model
 
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from udala.tablon import _
+from udala.tablon import _
 
 
 class IDocumentoTablon(model.Schema):
     """ Marker interface and Dexterity Python Schema for DocumentoTablon
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    origin = schema.TextLine(title=_(u"Origen del documento"))
 
-    # model.load('documento_tablon.xml')
+    origin_department = schema.TextLine(title=_(u"Departamento origen del documento"))
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    origin_details = schema.TextLine(title=_(u"Detalles del origen del documento"))
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    publication_url = schema.TextLine(title=_(u"URL de publicacion del documento"))
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
 
 
 @implementer(IDocumentoTablon)

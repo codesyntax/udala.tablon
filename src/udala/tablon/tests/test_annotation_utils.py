32baa509f3badcd38269fcc2eeeeac84e2700d8e
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-07-01 14:15:51 +0200

    annotation tests

--- src/udala/tablon/tests/test_annotation_utils.py
+++ src/udala/tablon/tests/test_annotation_utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from plone.app.multilingual.interfaces import ITranslationManager
 from plone.app.testing import login
 from plone.app.testing import setRoles
 from plone.app.testing import SITE_OWNER_NAME
@@ -8,13 +9,15 @@ from plone.app.testing import TEST_USER_ID
 from plone.dexterity.utils import createContentInContainer
 from udala.tablon.testing import UDALA_TABLON_FUNCTIONAL_TESTING
 from udala.tablon.testing import UDALA_TABLON_INTEGRATION_TESTING
+from udala.tablon.utils import ANNOTATION_KEY
 from udala.tablon.utils import delete_document
 from udala.tablon.utils import get_document_by_uid_and_lang
 from udala.tablon.utils import get_documents
 from udala.tablon.utils import register_documents
+from zope.annotation.interfaces import IAnnotations
 from zope.component import getMultiAdapter
 from zope.interface.interfaces import ComponentLookupError
-from plone.app.multilingual.interfaces import ITranslationManager
+
 import unittest
 
 
@@ -39,11 +42,66 @@ class TestAnnotationUtils(unittest.TestCase):
             mydoc_eu = createContentInContainer(
                 self.eu_tablon, "DocumentoTablon", title=doc
             )
+
+            file_eu = createContentInContainer(
+                mydoc_eu,
+                "AcreditedFile",
+                title=f"file_{doc}",
+            )
             mydoc_es = createContentInContainer(
                 self.es_tablon, "DocumentoTablon", title=doc
             )
-            ITranslationManager(mydoc_eu).register_translation("es", mydoc_es)
+            file_es = createContentInContainer(
+                mydoc_es,
+                "AcreditedFile",
+                title=f"file_{doc}",
+            )
 
-    def test_empty_tablon(self):
-        self.assertEqual(len(self.eu_tablon.values()), 0)
-        self.assertEqual(len(self.es_tablon.values()), 0)
+            ITranslationManager(mydoc_eu).register_translation("es", mydoc_es)
+            ITranslationManager(file_eu).register_translation("es", file_es)
+
+    def test_register_document_to_annotation(self):
+
+        created_uuid = register_documents(
+            self.eu_tablon.doc1.UID(),
+            self.es_tablon.doc1.UID(),
+            [self.eu_tablon.doc1.file_doc1.UID()],
+            [self.es_tablon.doc1.file_doc1.UID()],
+        )
+
+        annotated = IAnnotations(self.portal)
+        annotations = annotated.get(ANNOTATION_KEY)
+        self.assertIn(created_uuid, annotations)
+
+    def test_get_registered_document(self):
+
+        created_uuid = register_documents(
+            self.eu_tablon.doc1.UID(),
+            self.es_tablon.doc1.UID(),
+            [self.eu_tablon.doc1.file_doc1.UID()],
+            [self.es_tablon.doc1.file_doc1.UID()],
+        )
+
+        documents = get_documents(created_uuid)
+
+        self.assertTrue(isinstance(documents, dict))
+
+        self.assertTrue(documents["eu"], self.eu_tablon.doc1.UID())
+        self.assertTrue(documents["es"], self.es_tablon.doc1.UID())
+
+        self.assertIn(self.eu_tablon.doc1.file_doc1.UID(), documents["files_eu"])
+        self.assertIn(self.es_tablon.doc1.file_doc1.UID(), documents["files_es"])
+
+    def test_get_document_by_uid_and_lang(self):
+        created_uuid = register_documents(
+            self.eu_tablon.doc1.UID(),
+            self.es_tablon.doc1.UID(),
+            [self.eu_tablon.doc1.file_doc1.UID()],
+            [self.es_tablon.doc1.file_doc1.UID()],
+        )
+
+        eu_document = get_document_by_uid_and_lang(self.eu_tablon.doc1.UID(), "eu")
+        self.assertEqual(eu_document, created_uuid)
+
+        es_document = get_document_by_uid_and_lang(self.es_tablon.doc1.UID(), "es")
+        self.assertEqual(es_document, created_uuid)

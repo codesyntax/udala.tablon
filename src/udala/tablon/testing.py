f3b364d31c0038a707e1b2bbec5f4416ea6f100d
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-07-01 12:42:45 +0200

    initial tests

--- src/udala/tablon/testing.py
+++ src/udala/tablon/testing.py
@@ -1,13 +1,43 @@
 # -*- coding: utf-8 -*-
+from plone.app.i18n.locales.interfaces import IContentLanguages
+from plone.app.i18n.locales.interfaces import IMetadataLanguages
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
+from plone.app.testing import login
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.testing import z2
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.testing import zope
+from Products.CMFPlone.utils import getToolByName
+from zope.component import getUtility
 
 import udala.tablon
+
+
+ENABLED_LANGUAGES = ["eu", "es"]
+
+
+def set_supported_languages(portal):
+    """Set supported languages to the same predictable set for all test layers."""
+    language_tool = getToolByName(portal, "portal_languages")
+    for lang in ENABLED_LANGUAGES:
+        language_tool.addSupportedLanguage(lang)
+
+
+def set_available_languages():
+    """Limit available languages to a small set.
+
+    We're doing this to avoid excessive language lists in dumped responses
+    for docs. Depends on our own ModifiableLanguages components
+    (see plone.restapi:testing profile).
+    """
+    getUtility(IContentLanguages).setAvailableLanguages(ENABLED_LANGUAGES)
+    getUtility(IMetadataLanguages).setAvailableLanguages(ENABLED_LANGUAGES)
 
 
 class UdalaTablonLayer(PloneSandboxLayer):
@@ -27,7 +57,17 @@ class UdalaTablonLayer(PloneSandboxLayer):
         self.loadZCML(package=udala.tablon)
 
     def setUpPloneSite(self, portal):
+        portal.acl_users.userFolderAddUser(
+            SITE_OWNER_NAME, SITE_OWNER_PASSWORD, ["Manager"], []
+        )
+        login(portal, SITE_OWNER_NAME)
+        setRoles(portal, TEST_USER_ID, ["Manager"])
+
         applyProfile(portal, "udala.tablon:default")
+        set_supported_languages(portal)
+        if portal.portal_setup.profileExists("plone.app.multilingual:default"):
+            applyProfile(portal, "plone.app.multilingual:default")
+        set_available_languages()
 
 
 UDALA_TABLON_FIXTURE = UdalaTablonLayer()
@@ -40,16 +80,6 @@ UDALA_TABLON_INTEGRATION_TESTING = IntegrationTesting(
 
 
 UDALA_TABLON_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(UDALA_TABLON_FIXTURE,),
+    bases=(UDALA_TABLON_FIXTURE, zope.WSGI_SERVER_FIXTURE),
     name="UdalaTablonLayer:FunctionalTesting",
-)
-
-
-UDALA_TABLON_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        UDALA_TABLON_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name="UdalaTablonLayer:AcceptanceTesting",
 )
---
+++ src/udala/tablon/tests/test_annotation_utils.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.app.testing import login
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.dexterity.utils import createContentInContainer
+from udala.tablon.testing import UDALA_TABLON_FUNCTIONAL_TESTING
+from udala.tablon.testing import UDALA_TABLON_INTEGRATION_TESTING
+from udala.tablon.utils import delete_document
+from udala.tablon.utils import get_document_by_uid_and_lang
+from udala.tablon.utils import get_documents
+from udala.tablon.utils import register_documents
+from zope.component import getMultiAdapter
+from zope.interface.interfaces import ComponentLookupError
+from plone.app.multilingual.interfaces import ITranslationManager
+import unittest
+
+
+class TestAnnotationUtils(unittest.TestCase):
+
+    layer = UDALA_TABLON_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+
+        login(self.portal, SITE_OWNER_NAME)
+
+        self.eu_tablon = createContentInContainer(
+            self.portal.eu, "Tablon", title="Iragarki Ohola"
+        )
+        self.es_tablon = createContentInContainer(
+            self.portal.es, "Tablon", title="Tablón de Anuncios"
+        )
+        ITranslationManager(self.eu_tablon).register_translation("es", self.es_tablon)
+
+    def test_empty_tablon(self):
+        self.assertEqual(len(self.eu_tablon.values()), 0)
+        self.assertEqual(len(self.es_tablon.values()), 0)

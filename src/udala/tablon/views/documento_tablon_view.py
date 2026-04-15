ea41237602f4a6cb02ad2d7d5d51a246be04d38e
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-04-15 18:28:01 +0200

    pdb

--- src/udala/tablon/views/documento_tablon_view.py
+++ src/udala/tablon/views/documento_tablon_view.py
@@ -26,10 +26,6 @@ class DocumentoTablonView(BrowserView):
         portal_url = portal.absolute_url()
         language = self.context.Language()
         items = []
-        import pdb
-
-        pdb.set_trace()
-        a = 1
 
         document_key = get_document_by_uid_and_lang(self.context.UID(), language)
         document = get_documents(document_key)

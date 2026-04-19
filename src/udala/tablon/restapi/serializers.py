e98d0d040162cfb06d0df2f920c74b68768c218c
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-04-19 19:45:02 +0200

    feat: restructure GET REST API output to dynamically return generic translations and files arrays matching the POST signature

--- src/udala/tablon/restapi/serializers.py
+++ src/udala/tablon/restapi/serializers.py
@@ -5,7 +5,6 @@ from plone.restapi.serializer.dxcontent import SerializeToJson
 from udala.tablon.annotations.document import get_document_by_uid_and_lang
 from udala.tablon.annotations.document import get_documents
 from udala.tablon.annotations.file import get_file
-from udala.tablon.annotations.resolve import resolve_plone_uid
 from udala.tablon.content.documento_tablon import IDocumentoTablon
 from udala.tablon.interfaces import IUdalaTablonLayer
 from udala.tablon.subscribers.utils import is_pam_enabled
@@ -19,7 +18,7 @@ import base64
 @implementer(ISerializeToJson)
 @adapter(IDocumentoTablon, IUdalaTablonLayer)
 class DocumentoTablonSerializeToJson(SerializeToJson):
-    def __call__(self, version=None, include_items=False):
+    def __call__(self, version=None, include_items=False):  # noqa: C901
         portal_url = api.portal.get().absolute_url()
         document_key = get_document_by_uid_and_lang(
             self.context.UID(), self.context.Language()
@@ -41,26 +40,43 @@ class DocumentoTablonSerializeToJson(SerializeToJson):
             for file_id in set(files):
                 file_uid_data = get_file(file_id)
                 if file_uid_data is not None:
-                    file_uid = resolve_plone_uid(file_uid_data, self.request)
+                    file_translations = file_uid_data.get("translations", {})
 
-                    if file_uid is not None:
-                        file_object = api.content.get(UID=file_uid)
-                        if file_object is not None:
-                            file_data = {
-                                "@id": f"{portal_url}/@tablon/{document_key}/{file_id}",
-                                "uuid": file_id,
-                                "title": file_object.Title(),
-                                "izenpe_url": file_object.url,
-                                "izenpe_content": get_file_contents(file_object.url),
-                            }
-                            if file_object.file is not None:
-                                file_data.update({
-                                    "filename": file_object.file.filename,
-                                    "contents": base64.urlsafe_b64encode(
-                                        file_object.file.data
-                                    ).decode(),
-                                })
-                            documents.append(file_data)
+                    if not file_translations:
+                        continue
+
+                    # Grab the base file for generic attributes (filename, contents)
+                    base_lang = next(iter(file_translations))
+                    base_file_uid = file_translations[base_lang]
+                    base_file_obj = api.content.get(UID=base_file_uid)
+
+                    if base_file_obj is not None:
+                        file_data = {
+                            "@id": f"{portal_url}/@tablon/{document_key}/{file_id}",
+                            "uuid": file_id,
+                            "titles": {},
+                            "izenpe_urls": {},
+                            "izenpe_contents": {},
+                        }
+
+                        if base_file_obj.file is not None:
+                            file_data.update({
+                                "filename": base_file_obj.file.filename,
+                                "contents": base64.urlsafe_b64encode(
+                                    base_file_obj.file.data
+                                ).decode(),
+                            })
+
+                        for f_lang, f_uid in file_translations.items():
+                            f_obj = api.content.get(UID=f_uid)
+                            if f_obj:
+                                file_data["titles"][f_lang] = f_obj.Title()
+                                file_data["izenpe_urls"][f_lang] = f_obj.url
+                                file_data["izenpe_contents"][f_lang] = (
+                                    get_file_contents(f_obj.url)
+                                )
+
+                        documents.append(file_data)
 
         result = {
             "@id": f"{portal_url}/@tablon/{document_key}",
@@ -72,15 +88,18 @@ class DocumentoTablonSerializeToJson(SerializeToJson):
             if hasattr(self.context.expires, "toZone")
             else self.context.expires().toZone("UTC").ISO8601(),
             "origin": self.context.origin,
+            "translations": {},
             "documents": documents,
         }
 
         # Dynamically inject language-specific fields for all available translations
         for lang, translated_obj in translations.items():
             if translated_obj:
-                result[f"origin_department_{lang}"] = translated_obj.origin_department
-                result[f"origin_details_{lang}"] = translated_obj.origin_details
-                result[f"description_{lang}"] = translated_obj.description
-                result[f"publication_url_{lang}"] = translated_obj.publication_url
+                result["translations"][lang] = {
+                    "origin_department": translated_obj.origin_department,
+                    "origin_details": translated_obj.origin_details,
+                    "description": translated_obj.description,
+                    "publication_url": translated_obj.publication_url,
+                }
 
         return result
